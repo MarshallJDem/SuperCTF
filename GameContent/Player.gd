a0ab71d4f63bc37808d6845e8dea295c30848587
Marshall Demirjian <marshalljdem@gmail.com>
2020-06-05 22:35:12 -0500

    You'd never guess but even MORE ghost trail fixes

--- GameContent/Player.gd
+++ GameContent/Player.gd
@@ -602,7 +602,6 @@ remotesync func create_ghost_trail(start, end):
 	$Teleport_Audio.play();
 	for i in range(6):
 		var node = Ghost_Trail.instance();
-		get_tree().get_root().get_node("MainScene").add_child(node);
 		node.position = start;
 		node.position.x = node.position.x + ((i) * (end.x - start.x)/4)
 		node.position.y = node.position.y + ((i) * (end.y - start.y)/4)
@@ -617,6 +616,7 @@ remotesync func create_ghost_trail(start, end):
 		node.get_node("Sprite_Body").z_index = $Sprite_Body.z_index
 		node.get_node("Sprite_Legs").texture = $Sprite_Legs.texture
 		node.get_node("Sprite_Legs").frame = $Sprite_Legs.frame
+		get_tree().get_root().get_node("MainScene").add_child(node);
 	# If this is a puppet, use this ghost trail as an oppurtunity to also update its position
 	if !is_network_master():
 		lerp_start_pos = end;
