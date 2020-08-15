516deca88f034ac7e99f6b5380ecd3579a17fee2
Marshall Demirjian <marshalljdem@gmail.com>
2020-08-15 13:08:43 -0700

    Powerup fixes

--- GameContent/Powerup_Spawner.gd
+++ GameContent/Powerup_Spawner.gd
@@ -11,17 +11,16 @@ func _ready():
 		$Spawner_Timer.wait_time = 5;
 		var n = randi()%4+1; #%11+1 means random number 1-10
 		spawn_powerup(n);
-	if get_tree().is_network_server():
-		$Spawner_Timer.start();
+	$Spawner_Timer.start();
 
 func _process(delta):
-	
 	if $Spawner_Timer.time_left == 0:
 		$Text.bbcode_text = "";
 	else:
 		$Text.bbcode_text = "[center][color=black]" + str(int($Spawner_Timer.time_left) + 1);
 
 remotesync func spawn_powerup(n):
+	print("SPAWNING POWERUP");
 	var powerup = Powerup.instance();
 	powerup.position = position + Vector2(0, -15);
 	powerup.type = n;
