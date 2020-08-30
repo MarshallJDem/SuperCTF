2b7658d206f41e3aaff25188b50859c455aa5cab
Marshall Demirjian <marshalljdem@gmail.com>
2020-08-30 11:25:31 -0700

    UI Tweak

--- Stats_View_Cell.gd
+++ Stats_View_Cell.gd
@@ -9,9 +9,4 @@ var players;
 func _ready():
 	$Player_Stats.bbcode_text = "[center]" + str(stats[player_id]["kills"]) + "\n\n" + str(stats[player_id]["deaths"]) + "\n\n" + str(stats[player_id]["captures"]) + "\n\n" + str(stats[player_id]["recovers"]);
 	$Player_Name.bbcode_text = "[center]" + str(players[player_id]["name"]);
-	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
