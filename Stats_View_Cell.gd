dd7b24e180ebea2a990fe4383283719ad8596b78
Marshall Demirjian <marshalljdem@gmail.com>
2020-08-30 11:21:03 -0700

    UI Tweak

--- Stats_View_Cell.gd
+++ Stats_View_Cell.gd
@@ -8,7 +8,7 @@ var players;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Player_Stats.bbcode_text = "[center]" + str(stats[player_id]["kills"]) + "\n\n" + str(stats[player_id]["deaths"]) + "\n\n" + str(stats[player_id]["captures"]) + "\n\n" + str(stats[player_id]["recovers"]);
-	$Player_Name.bbcode_text = str(players[player_id]["name"]);
+	$Player_Name.bbcode_text = "[center]" + str(players[player_id]["name"]);
 	
 
 
