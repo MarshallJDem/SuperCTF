e5f141f49f3e7347e717dc2681d4705a2099b02b
Marshall Demirjian <marshalljdem@gmail.com>
2020-09-15 22:26:42 -0700

    Results screen should now be receptive to team

--- Stats_View_Cell.gd
+++ Stats_View_Cell.gd
@@ -9,4 +9,16 @@ var players;
 func _ready():
 	$Player_Stats.bbcode_text = "[center]" + str(stats[player_id]["kills"]) + "\n\n" + str(stats[player_id]["deaths"]) + "\n\n" + str(stats[player_id]["captures"]) + "\n\n" + str(stats[player_id]["recovers"]);
 	$Player_Name.bbcode_text = "[center]" + str(players[player_id]["name"]);
+	var c = players[player_id]["class"];
+	var t = "B";
+	if(players[player_id]["team_id"] == 1):
+		t = "R";
+	var n;
+	if c == Globals.Classes.Bullet:
+		n = "gunner";
+	elif c == Globals.Classes.Laser:
+		n = "laser";
+	elif c == Globals.Classes.Demo:
+		n = "demo";
+	$Player_Sprite.set_texture(load("res://Assets/Player/" + str(n) + "_head_" +t+ ".png"));
 
