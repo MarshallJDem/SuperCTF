4904274dbc212f9a48035b05d3d1b8a0b1ec5c9e
Marshall Demirjian <marshalljdem@gmail.com>
2020-09-16 12:19:12 -0700

    Extended match end timer and added visual disables when it runs out in game results screen

--- Game_Results_Screen.gd
+++ Game_Results_Screen.gd
@@ -164,5 +164,8 @@ func _process(_delta):
 	
 	# Time left
 	var timeleft = int(get_parent().get_node("NetworkController/Match_End_Timer").time_left);
-	
 	$CanvasLayer/Control/Time_Left_Text.text = str(timeleft if timeleft > 0 else "Server Disconnected");
+	if timeleft <= 0:
+		$CanvasLayer/Control/DD_Description.modulate = Color(0.5,0.5,0.5,0.5);
+		$CanvasLayer/Control/DD_Votes.modulate = Color(0.5,0.5,0.5,0.5);
+		$CanvasLayer/Control/Button_Rematch.disabled = true;
