31f51a3bf6a33ad92a9beb7feea02e01d06a8945
Marshall Demirjian <marshalljdem@gmail.com>
2020-09-20 18:57:42 -0700

    changed the way laser collision boxes work again

--- GameContent/Laser.gd
+++ GameContent/Laser.gd
@@ -22,7 +22,8 @@ func _ready():
 		$Area2D.monitoring = true;
 	
 	$Area2D.rotation = Vector2(0,0).angle_to_point(target_pos) + PI/2;
-	$Area2D/CollisionShape2D.scale = Vector2(size/3, Vector2(0,0).distance_to(target_pos));
+	$Area2D/CollisionShape2D.shape.set_extents(Vector2(size/2, Vector2(0,0).distance_to(target_pos)/2));
+	$Area2D/CollisionShape2D.position.y = Vector2(0,0).distance_to(target_pos)/2;
 	print(is_blank);
 	
 func _physics_process(delta):
