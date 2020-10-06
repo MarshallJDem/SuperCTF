ee8065b2d60b8ffc6529e4433f2f06ecf62ad67d
Marshall Demirjian <marshalljdem@gmail.com>
2020-10-05 17:28:44 -0700

    Added shoot stick

--- GameContent/Move_Stick.gd
+++ GameContent/Move_Stick.gd
@@ -4,16 +4,24 @@ var radius_big = 125;
 var radius_small = 50;
 var origin = Vector2(0,0);
 var margin = Vector2(75,150);
+export var is_move = true;
 # This is the input vector of the stick
 # Its max magnitude is radius_big
 var stick_vector = Vector2(0,0);
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(_delta):
-	origin = Vector2((radius_big + margin.x), rect_size.y - margin.y);
+	radius_big = OS.get_window_size().y/6
+	radius_small = radius_big / 3;
+	if is_move:
+		origin = Vector2((radius_big + margin.x), rect_size.y - margin.y);
+	else:
+		origin = Vector2(rect_size.x - (radius_big + margin.x), rect_size.y - margin.y);
+		
 	update();
 	
 func _draw():
