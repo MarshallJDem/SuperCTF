0378c6925892b14dcb1c1fe10a98427f876a4d85
MarshallJDem <marshalljdem@gmail.com>
2020-11-19 00:13:42 -0800

    Dash powerup buffed

--- GameContent/Player.gd
+++ GameContent/Player.gd
@@ -301,7 +301,7 @@ remotesync func enable_powerup(type):
 		$Ability_Node.ability_stacks += 1;
 		text = "[wave amp=50 freq=12][color=red]^^ +1 INSTANT ABILITY USE ^^";
 	elif type == 4:
-		DASH_COOLDOWN_PMODIFIER = -1.5;
+		DASH_COOLDOWN_PMODIFIER = -2.5;
 		$Powerup_Timer.wait_time = 10;
 		text = "[wave amp=50 freq=12][color=purple]˅˅˅˅˅˅^^ DASH RATE UP ^^";
 	if Globals.testing or is_network_master():
