65afc4856ddd051628d9aaf1acc12536f7952a01
MarshallJDem <marshalljdem@gmail.com>
2021-01-03 17:17:08 -0600

    Disabled testing mode

--- Globals.gd
+++ Globals.gd
@@ -1,8 +1,8 @@
 extends Node
 
 # Whether to run in testing mode (for development uses)
-var testing = true;
-var experimental = true;
+var testing = false;
+var experimental = false;
 var temporaryQuickplayDisable = true;
 var localTesting = false; # Used for running a server locally on the machine
 
