12be09f41b7f8c8fb41e10cecbb40d0a81d63a89
Marshall Demirjian <marshalljdem@gmail.com>
2021-02-11 14:20:11 -0800

    Fixed create guest and login API calls

--- Globals.gd
+++ Globals.gd
@@ -1,10 +1,10 @@
 extends Node
 
 # Whether to run in testing mode (for development uses)
-var testing = true;
+var testing = false;
 var experimental = true;
 var localTesting = false; # Used for running a server locally on the machine
-var localTestingBackend = false; # Used for when the backend is running locally on this machine
+var localTestingBackend = true; # Used for when the backend is running locally on this machine
 var remoteSkirmish = false; # Used for running the skirmish lobby on a remote computer (so you can run it in the editor and catch bugs)
 
 var temporaryQuickplayDisable = true;
