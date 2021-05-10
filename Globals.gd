08050596cf5a0b931f390bc260fb5b5d9ef81d02
Marshall Demirjian <marshalljdem@gmail.com>
2021-05-09 22:13:26 -0700

    Updated testing flags

--- Globals.gd
+++ Globals.gd
@@ -1,7 +1,7 @@
 extends Node
 
 # Whether to run in testing mode (for development uses)
-var testing = false;
+var testing = true;
 var experimental = true;
 var localTesting = false; # Used for running a gameserver locally on the machine
 var localTestingBackend = false; # Used for when the backend is running locally on this machine
