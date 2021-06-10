93a1eb4637c038fb566afa9635ff3200d0ca8d67
JamJawJar <66331482+JamJawJar@users.noreply.github.com>
2021-06-10 21:09:40 +0100

    Fix scoreboard code
    -Now with added comments!
    -New Recipe (less errors same taste!)

--- GameContent/ScoreBoard.gd
+++ GameContent/ScoreBoard.gd
@@ -31,98 +31,121 @@ func setup():
 	
 	fetch_data()
 	
+	var player
+	var stat
+	
 	#check if all players have stats
 	if len(players_data) == len(stats_data):
+		
+		for player_number in players_data:
+			player = players_data.get(player_number)
+			stat = stats_data.get(player_number)
 	
-		for current_player in len(players_data):
-			current_player += 1
-			if players_data[str(current_player)]["team_id"] == 0:
+			if player["team_id"] == 0:
 				
 				#make a label for player name and add it to the scoreboard
 				var player_label = make_new_label("0e00ff")
-				player_label.text = players_data[str(current_player)]["name"]
+				player_label.text = player["name"]
 				$MarginContainer/Teams/Team_1/Columns/Name_Column.add_child(player_label)
 				
 				#make a label for player kills and add it to the scoreboard
 				player_label = make_new_label("0e00ff")
-				player_label.text = str(stats_data[str(current_player)]["kills"])
+				player_label.text = str(stat["kills"])
 				$MarginContainer/Teams/Team_1/Columns/Kills_Column.add_child(player_label)
 				
 				#make a label for player deaths and add it to the scoreboard
 				player_label = make_new_label("0e00ff")
-				player_label.text = str(stats_data[str(current_player)]["deaths"])
+				player_label.text = str(stat["deaths"])
 				$MarginContainer/Teams/Team_1/Columns/Deaths_Column.add_child(player_label)
 				
 				#make a label for player captures and add it to the scoreboard
 				player_label = make_new_label("0e00ff")
-				player_label.text = str(stats_data[str(current_player)]["captures"])
+				player_label.text = str(stat["captures"])
 				$MarginContainer/Teams/Team_1/Columns/Captures_Column.add_child(player_label)
 				
 				#make a label for player recovers and add it to the scoreboard
 				player_label = make_new_label("0e00ff")
-				player_label.text = str(stats_data[str(current_player)]["recovers"])
+				player_label.text = str(stat["recovers"])
 				$MarginContainer/Teams/Team_1/Columns/Recovers_Column.add_child(player_label)
 			
-			elif players_data[str(current_player)]["team_id"] == 1:
+			elif player["team_id"] == 1:
 				
 				#make a label for player name and add it to the scoreboard
 				var player_label = make_new_label("ff0000")
-				player_label.text = players_data[str(current_player)]["name"]
+				player_label.text = player["name"]
 				$MarginContainer/Teams/Team_2/Columns2/Name_Column2.add_child(player_label)
 				
 				#make a label for player kills and add it to the scoreboard
 				player_label = make_new_label("ff0000")
-				player_label.text = str(stats_data[str(current_player)]["kills"])
+				player_label.text = str(stat["kills"])
 				$MarginContainer/Teams/Team_2/Columns2/Kills_Column2.add_child(player_label)
 				
 				#make a label for player deaths and add it to the scoreboard
 				player_label = make_new_label("ff0000")
-				player_label.text = str(stats_data[str(current_player)]["deaths"])
+				player_label.text = str(stat["deaths"])
 				$MarginContainer/Teams/Team_2/Columns2/Deaths_Column2.add_child(player_label)
 				
 				#make a label for player captures and add it to the scoreboard
 				player_label = make_new_label("ff0000")
-				player_label.text = str(stats_data[str(current_player)]["captures"])
+				player_label.text = str(stat["captures"])
 				$MarginContainer/Teams/Team_2/Columns2/Captures_Column2.add_child(player_label)
 				
 				#make a label for player recovers and add it to the scoreboard
 				player_label = make_new_label("ff0000")
-				player_label.text = str(stats_data[str(current_player)]["recovers"])
+				player_label.text = str(stat["recovers"])
 				$MarginContainer/Teams/Team_2/Columns2/Recovers_Column2.add_child(player_label)
 				
 			else:
-				print("error player had team id "+str(players_data[str(current_player)]["team_id"]))
+				print("error player had team id "+str(player["team_id"]))
 	else:
-		print("Error player_data and stat_data are not the same size")
+		print("Error player_data and stat_data are not the same size (setup function)")
 
 #refresh the score board to the current stats of the stats_data dictionary
 func _refresh():
 	
 	fetch_data()
 	
-	#counts how many blue players there are
-	var blue_players = 0
-	#counts how many red players there are
-	var red_players = 0
-	#go through all players
-	for current_player in len(stats_data):
-		current_player += 1
-		#if player is blue
-		if players_data[str(current_player)]["team_id"] == 0:
-			blue_players += 1
-			#set all stats to current stats_data stats
-			$MarginContainer/Teams/Team_1/Columns/Kills_Column.get_child(blue_players).text = str(stats_data[str(current_player)]["kills"])
-			$MarginContainer/Teams/Team_1/Columns/Deaths_Column.get_child(blue_players).text = str(stats_data[str(current_player)]["deaths"])
-			$MarginContainer/Teams/Team_1/Columns/Captures_Column.get_child(blue_players).text = str(stats_data[str(current_player)]["captures"])
-			$MarginContainer/Teams/Team_1/Columns/Recovers_Column.get_child(blue_players).text = str(stats_data[str(current_player)]["recovers"])
-		#if player is red
-		elif players_data[str(current_player)]["team_id"] == 1:
-			red_players += 1
-			#set all stats to current stats_data stats
-			$MarginContainer/Teams/Team_2/Columns2/Kills_Column2.get_child(red_players).text = str(stats_data[str(current_player)]["kills"])
-			$MarginContainer/Teams/Team_2/Columns2/Deaths_Column2.get_child(red_players).text = str(stats_data[str(current_player)]["deaths"])
-			$MarginContainer/Teams/Team_2/Columns2/Captures_Column2.get_child(blue_players).text = str(stats_data[str(current_player)]["captures"])
-			$MarginContainer/Teams/Team_2/Columns2/Recovers_Column2.get_child(blue_players).text = str(stats_data[str(current_player)]["recovers"])
+	#check if all players have stats
+	if len(players_data) == len(stats_data):
+		
+		var player
+		var stat
+		#counts how many blue players there are
+		var blue_players = 0
+		#counts how many red players there are
+		var red_players = 0
+		
+		
+		#go through all players
+		for player_number in players_data:
+			#get player info dictionary of the selected player
+			player = players_data.get(player_number)
+			#get stats dictionary of that player
+			stat = stats_data.get(player_number)
+			
+			
+			#if player is blue
+			if player["team_id"] == 0:
+				blue_players += 1
+				#set all stats to current stats_data stats
+				$MarginContainer/Teams/Team_1/Columns/Kills_Column.get_child(blue_players).text = str(stat["kills"])
+				$MarginContainer/Teams/Team_1/Columns/Deaths_Column.get_child(blue_players).text = str(stat["deaths"])
+				$MarginContainer/Teams/Team_1/Columns/Captures_Column.get_child(blue_players).text = str(stat["captures"])
+				$MarginContainer/Teams/Team_1/Columns/Recovers_Column.get_child(blue_players).text = str(stat["recovers"])
+			#if player is red
+			elif player["team_id"] == 1:
+				red_players += 1
+				#set all stats to current stats_data stats
+				$MarginContainer/Teams/Team_2/Columns2/Kills_Column2.get_child(red_players).text = str(stat["kills"])
+				$MarginContainer/Teams/Team_2/Columns2/Deaths_Column2.get_child(red_players).text = str(stat["deaths"])
+				$MarginContainer/Teams/Team_2/Columns2/Captures_Column2.get_child(blue_players).text = str(stat["captures"])
+				$MarginContainer/Teams/Team_2/Columns2/Recovers_Column2.get_child(blue_players).text = str(stat["recovers"])
+				
+				
+	else:
+		print("Error player_data and stat_data are not the same size (refresh function)")
+
+
 
 func _screen_resized():
 	var window_size = OS.get_window_size();
@@ -143,5 +166,7 @@ func fetch_data():
 	print("scoreboard stats")
 	print(nc.game_stats)
 	print(stats_data)
+	print("Player stats:")
+	print(players_data)
 	players_data = nc.players
 
