436e76ac438ee1134ca35031a66574d5e4a5c5d5
JamJawJar <66331482+JamJawJar@users.noreply.github.com>
2021-06-23 20:36:47 +0100

    Emote now fades
    Emote appears, then fades on button press

--- Emote.gd
+++ Emote.gd
@@ -2,23 +2,42 @@ extends Node2D
 
 #how long the emote will show
 var emote_time_length = 2
+var fading = false
 
 func _ready() -> void:
-	$"Still Emote".connect("timeout", self, "_stop_emote")
+	$"Emote Timer".connect("timeout", self, "_stop_emote")
+	$"Fade Timer".connect("timeout", self, "_stop_fade")
 
 func _emote(emote_number):
 	$"Emote Timer".wait_time = emote_time_length
 	$"Emote Timer".start()
 	
 	$"Still Emote".visible = true
-	
-	pass
+	$"Still Emote".modulate.a = 1
+
+
 func _stop_emote():
-	$"Still Emote".visible = false
+	fading = true
+	$"Fade Timer".start()
 	
 func _process(delta: float) -> void:
-	if emote_time_length != 0:
-		var time_left_decimal = $"Emote Timer".time_left/emote_time_length
-	
-		if time_left_decimal < 0.1:
-			$"Still Emote".modulate.a = time_left_decimal
+	#make button press activate _emote()
+	if Input.is_action_just_pressed("score_board"):
+		_emote(1)
+	print($"Emote Timer".time_left)
+	if fading:
+		print($"Fade Timer".time_left)
+		if $"Fade Timer".wait_time != 0:
+			$"Still Emote".modulate.a = $"Fade Timer".time_left/$"Fade Timer".wait_time
+		
+		
+		
+#		if emote_time_length != 0:
+#			var time_left_decimal = $"Emote Timer".time_left/emote_time_length
+#			print(time_left_decimal)
+#			if time_left_decimal < 0.3:
+#				$"Still Emote".modulate.a = time_left_decimal * 3
+
+func _stop_fade():
+	$"Still Emote".visible = false
+	fading =false
