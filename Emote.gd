9b7666746eb7c6762a84039e1fac2e99c8ffdc32
JamJawJar <66331482+JamJawJar@users.noreply.github.com>
2021-06-27 13:24:56 +0100

    Added animation to emotes
    they now bob up and down, also added code to fetch emotes from emote folder

--- Emote.gd
+++ Emote.gd
@@ -3,32 +3,46 @@ extends Node2D
 #how long the emote will show
 var emote_time_length = 2
 var fading = false
-
+var emoting = false
 func _ready() -> void:
-	$"Emote Timer".connect("timeout", self, "_stop_emote")
-	$"Fade Timer".connect("timeout", self, "_stop_fade")
-
-func _emote(emote_number):
-	$"Emote Timer".wait_time = emote_time_length
-	$"Emote Timer".start()
+	$"Emote Timer".connect("timeout", self, "_fade_emote")
+	$"Fade Timer".connect("timeout", self, "_stop_emote")
 	
-	$"Still Emote".visible = true
-	$"Still Emote".modulate.a = 1
-
-
-func _stop_emote():
-	fading = true
-	$"Fade Timer".start()
+	
 	
 func _process(delta: float) -> void:
 	#make button press activate _emote()
 	if Input.is_action_just_pressed("score_board"):
+		_emote(2)
+	if Input.is_action_just_pressed("clickL"):
 		_emote(1)
 	print($"Emote Timer".time_left)
+	
 	if fading:
 		print($"Fade Timer".time_left)
 		if $"Fade Timer".wait_time != 0:
 			$"Still Emote".modulate.a = $"Fade Timer".time_left/$"Fade Timer".wait_time
+	
+	
+	
+#load the emote and then emote it
+func _emote(emote_number):
+	if not emoting:
+		emoting = true
+		$"Still Emote".texture = load("res://Assets/Emotes/emote_"+ str(emote_number)+ ".png")
+		$"Emote Timer".wait_time = emote_time_length
+		$"Emote Timer".start()
+		$AnimationPlayer.play("Emote_bounce")
+		
+		$"Still Emote".modulate.a = 1
+		$"Still Emote".visible = true
+	
+#start emote fading
+func _fade_emote():
+	fading = true
+	$"Fade Timer".start()
+	
+
 		
 		
 		
@@ -38,6 +52,8 @@ func _process(delta: float) -> void:
 #			if time_left_decimal < 0.3:
 #				$"Still Emote".modulate.a = time_left_decimal * 3
 
-func _stop_fade():
+#Make emote invisible
+func _stop_emote():
 	$"Still Emote".visible = false
-	fading =false
+	fading = false
+	emoting = false
