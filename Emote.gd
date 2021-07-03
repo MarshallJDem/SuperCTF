a64c78f2737756f8dd9731e8b654e04625e95beb
JamJawJar <66331482+JamJawJar@users.noreply.github.com>
2021-07-03 22:13:17 +0100

    Emotes can now vary in length of time

--- Emote.gd
+++ Emote.gd
@@ -1,21 +1,16 @@
 extends Node2D
 
-#how long the emote will show
-var emote_time_length : float = 3
+#default amount of time for the emote to appear then disappear
+var default_emote_time_length : float = 3
 var fading = false
 var emoting = false
+
+
 func _ready() -> void:
 	$"Emote Timer".connect("timeout", self, "_fade_emote")
 	$"Fade Timer".connect("timeout", self, "_stop_emote")
 	
-	#Make sure the timers and animation speed match the emote time length
-	if emote_time_length != 0:
-		$AnimationPlayer.playback_speed = 1 / emote_time_length 
-	print(emote_time_length)
-	$"Emote Timer".wait_time = (2.0/3.0)*emote_time_length
-	print("emote length = "+str($"Emote Timer".wait_time))
-	$"Fade Timer".wait_time = (1.0/3.0)*emote_time_length
-	print($"Fade Timer".wait_time)
+	
 	
 func _process(delta: float) -> void:
 	#make button press activate _emote()
@@ -25,15 +20,15 @@ func _process(delta: float) -> void:
 		_emote(1)
 	
 	if fading:
-		print($"Fade Timer".time_left)
 		if $"Fade Timer".wait_time != 0:
 			$"Still Emote".modulate.a = $"Fade Timer".time_left/$"Fade Timer".wait_time
 	
 	
 	
 #load the emote and then emote it
-func _emote(emote_number):
+func _emote(emote_number, emote_length : float = default_emote_time_length ):
 	if not emoting:
+		set_emote_timing(emote_length)
 		emoting = true
 		$"Still Emote".texture = load("res://Assets/Emotes/emote_"+ str(emote_number)+ ".png")
 		
@@ -52,14 +47,22 @@ func _fade_emote():
 		
 		
 		
-#		if emote_time_length != 0:
-#			var time_left_decimal = $"Emote Timer".time_left/emote_time_length
-#			print(time_left_decimal)
-#			if time_left_decimal < 0.3:
-#				$"Still Emote".modulate.a = time_left_decimal * 3
+
 
 #Make emote invisible
 func _stop_emote():
 	$"Still Emote".visible = false
 	fading = false
 	emoting = false
+	
+
+
+func set_emote_timing(time):
+	#Make sure the timers and animation speed match the emote time length
+	if time != 0:
+		$AnimationPlayer.playback_speed = 1 / time 
+	
+	$"Emote Timer".wait_time = (2.0/3.0)*time
+	
+	$"Fade Timer".wait_time = (1.0/3.0)*time
+	
